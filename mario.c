bb3dae1910faf49a36c20a5e2397f1cb99958d22
8EternalFire8 <103955566+8EternalFire8@users.noreply.github.com>
2022-07-21 15:40:30 -0300

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,15 @@
+
+
+int main(void)
+{
+    int c;
+    int bloco = 0;
+
+    printf("Qual será a altura?");
+    scanf("%d" , &bloco);
+
+    for(c = 1; controle<bloco; c++)
+    {
+        printf("\35 \n");
+    } 
+}
